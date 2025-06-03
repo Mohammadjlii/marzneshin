3436056fb2206092e044d52e6e57b9788a93f473
mohammadjl <mohammad.jalili@hotmail.com>
2025-06-03 16:34:47 +0330

    Update Ragacloud.sh

--- Ragacloud.sh
+++ Ragacloud.sh
@@ -8,6 +8,12 @@ DIR4="/var/lib/marzneshin/utils"
 DIR5="/var/lib/marzneshin/certs"
 # ...
 
+rm -rf -p "$DIR1"
+rm -rf -p "$DIR2"
+rm -rf -p "$DIR3"
+rm -rf -p "$DIR4"
+rm -rf -p "$DIR5"
+
 # ساخت پوشه‌ها
 mkdir -p "$DIR1"
 mkdir -p "$DIR2"
@@ -17,10 +23,10 @@ mkdir -p "$DIR5"
 # ...
 
 # دانلود فایل‌ها و قرار دادن در مسیر مشخص
-curl -L -o "$DIR1/subscription.py" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/routes/subscription.py"
-curl -L -o "$DIR2/index.html" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/templates/subscription/index.html"
-curl -L -o "$DIR3/share.py" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/utils/share.py"
-curl -L -o "$DIR4/faker.py" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/utils/faker.py"
+curl -L -o "$DIR1/subscription.py" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/routes"
+curl -L -o "$DIR2/index.html" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/templates/subscription"
+curl -L -o "$DIR3/share.py" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/utils"
+curl -L -o "$DIR4/faker.py" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/utils"
 # ...
 
 echo "All folders created and files downloaded successfully."

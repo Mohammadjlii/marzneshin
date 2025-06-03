114a2d49dff9fa712f4df2fdc1fa317947510c15
mohammadjl <mohammad.jalili@hotmail.com>
2025-06-03 18:06:04 +0330

    Update Ragacloud.sh

--- Ragacloud.sh
+++ Ragacloud.sh
@@ -30,6 +30,77 @@ curl -L -o "$DIR4/system.py" "https://raw.githubusercontent.com/Mohammadjlii/mar
 
 echo "All folders created and files downloaded successfully."
 
+#Install .env
+
+ENV_PATH="/etc/opt/marzneshin/.env"
+
+# حذف فایل قبلی اگر وجود دارد
+if [ -f "$ENV_PATH" ]; then
+    rm "$ENV_PATH"
+    echo -e "\e[32mOld docker-compose.yml removed.\e[0m"
+fi
+
+# ساخت فایل جدید env
+cat << EOF > "$ENV_PATH"
+
+UVICORN_HOST = "0.0.0.0"
+UVICORN_PORT = 2096
+
+## THE FOLLOWING TWO VARIABLES ARE NOT SUPPORTED ANYMORE, USE merzneshin cli.
+ SUDO_USERNAME = "Mohammad"
+ SUDO_PASSWORD = "Mohammad381169@!"
+
+ DASHBOARD_PATH = "/Ragacloud/"
+
+# UVICORN_UDS: "/run/marzneshin.socket"
+ UVICORN_SSL_CERTFILE = "/var/lib/marzneshin/certs/cert.crt"
+ UVICORN_SSL_KEYFILE = "/var/lib/marzneshin/certs/private.key"
+
+
+# SUBSCRIPTION_URL_PREFIX = "https://example.com"
+
+
+# TELEGRAM_API_TOKEN = 123456789:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+# TELEGRAM_ADMIN_ID = 987654321, 123456789
+# TELEGRAM_LOGGER_CHANNEL_ID = -1234567890123
+# TELEGRAM_PROXY_URL = "http://localhost:8080"
+
+# DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/xxxxxxx"
+
+ CUSTOM_TEMPLATES_DIRECTORY="/var/lib/marzneshin/templates/"
+# CLASH_SUBSCRIPTION_TEMPLATE="/var/lib/marzneshin/templates/clash.yml"
+# SINGBOX_SUBSCRIPTION_TEMPLATE="/var/lib/marzneshin/templates/sing-box.json"
+ XRAY_SUBSCRIPTION_TEMPLATE="/var/lib/marzneshin/templates/xray.json"
+ SUBSCRIPTION_PAGE_TEMPLATE="subscription/index.html"
+# HOME_PAGE_TEMPLATE="home/index.html"
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///db.sqlite3"
+# SQLALCHEMY_CONNECTION_POOL_SIZE = 10
+# SQLALCHEMY_CONNECTION_MAX_OVERFLOW = -1
+
+### for developers
+# DOCS=true
+# DEBUG=true
+# WEBHOOK_ADDRESS = "http://127.0.0.1:9000/"
+# WEBHOOK_SECRET = "something-very-very-secret"
+# VITE_BASE_API="https://example.com/api/"
+# JWT_ACCESS_TOKEN_EXPIRE_MINUTES = 1440
+# AUTH_GENERATION_ALGORITHM=xxh128
+
+## Scheduled Tasks Interval in Seconds
+# TASKS_RECORD_USER_USAGES_INTERVAL=30
+# TASKS_REVIEW_USERS_INTERVAL=30
+# TASKS_EXPIRE_DAYS_REACHED_INTERVAL=30
+# TASKS_RESET_USER_DATA_USAGE=3600
+
+EOF
+
+echo -e "\e[32mNew docker-compose.yml created successfully.\e[0m"
+
+
+
+# install docker-compose
+
 COMPOSE_PATH="/etc/opt/marzneshin/docker-compose.yml"
 
 # حذف فایل قبلی اگر وجود دارد

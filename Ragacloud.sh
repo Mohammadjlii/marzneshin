3a254771836d1006ad6f728ee002c8418e67de3d
mohammadjl <mohammad.jalili@hotmail.com>
2025-06-03 16:55:27 +0330

    Update Ragacloud.sh

--- Ragacloud.sh
+++ Ragacloud.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# تعریف مسیرها
 DIR1="/var/lib/marzneshin/routes"
 DIR2="/var/lib/marzneshin/subscription"
 DIR3="/var/lib/marzneshin/templates"
 DIR4="/var/lib/marzneshin/utils"
 DIR5="/var/lib/marzneshin/certs"
+# ...
 
 rm -rf "$DIR1"
 rm -rf "$DIR2"
@@ -12,24 +14,32 @@ rm -rf "$DIR3"
 rm -rf "$DIR4"
 rm -rf "$DIR5"
 
+# ساخت پوشه‌ها
 mkdir -p "$DIR1"
 mkdir -p "$DIR2"
 mkdir -p "$DIR3"
 mkdir -p "$DIR4"
 mkdir -p "$DIR5"
+# ...
 
-curl -L -o "$DIR1/subscription.py" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/routes/subscription.py"
-curl -L -o "$DIR2/index.html" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/templates/subscription/index.html"
-curl -L -o "$DIR4/share.py" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/utils/share.py"
-curl -L -o "$DIR4/faker.py" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/utils/faker.py"
+# دانلود فایل‌ها و قرار دادن در مسیر مشخص
+wget "$DIR1" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/routes/subscription.py"
+wget "$DIR2" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/templates/subscription/index.html"
+wget "$DIR3" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/utils/share.py"
+wget "$DIR4" "https://raw.githubusercontent.com/Mohammadjlii/marzneshin/master/app/utils/faker.py"
+# ...
+
+echo "All folders created and files downloaded successfully."
 
 COMPOSE_PATH="/etc/opt/marzneshin/docker-compose.yml"
 
+# حذف فایل قبلی اگر وجود دارد
 if [ -f "$COMPOSE_PATH" ]; then
     rm "$COMPOSE_PATH"
     echo -e "\e[32mOld docker-compose.yml removed.\e[0m"
 fi
 
+# ساخت فایل جدید docker-compose.yml
 cat << EOF > "$COMPOSE_PATH"
 services:
   marzneshin:
@@ -41,8 +51,9 @@ services:
       SQLALCHEMY_DATABASE_URL: "sqlite:////var/lib/marzneshin/db.sqlite3"
     volumes:
      - /var/lib/marzneshin/:/var/lib/marzneshin/
-     - /var/lib/marzneshin/utils:/app/app/utils
-     - /var/lib/marzneshin/routes:/app/app/routes
+     - /var/lib/marzneshin/utils/share.py:/app/app/utils/share.py
+     - /var/lib/marzneshin/utils/faker.py:/app/app/utils/faker.py
+     - /var/lib/marzneshin/routes/subscription.py:/app/app/routes/subscription.py
 
   marznode:
     image: dawsh/marznode:latest
@@ -62,8 +73,8 @@ EOF
 
 echo -e "\e[32mNew docker-compose.yml created successfully.\e[0m"
 
-docker-compose -f /etc/opt/marzneshin/docker-compose.yml down
-docker-compose -f /etc/opt/marzneshin/docker-compose.yml up -d
+# ری‌استارت کردن سرویس marzneshin با docker-compose
+marzneshin restart
 
 if [ $? -eq 0 ]; then
   echo -e "\e[32mdocker-compose restarted marzneshin successfully.\e[0m"
